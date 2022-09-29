b78c49c53b2a887f24a9e41ee36f2c162841ae11
Evann Berthou <evann.berthou@gmail.com>
2022-09-29 20:49:04 +0200

    Fix de l'affichage des réposnes (type multiple)

--- src/app/Modeles/REPONSE.ts
+++ src/app/Modeles/REPONSE.ts
@@ -1,18 +1,20 @@
-import {IOption} from "./OPTION";
+import { IOption } from "./OPTION";
 
-export interface IReponse{
+export interface IReponse {
   texte: string;
   id?: number;
   isGood: boolean;
+  isRight: boolean;
 }
-export class Reponse implements IReponse{
+export class Reponse implements IReponse {
   texte: string;
   id: number | undefined;
-  isGood : boolean;
-  constructor(texte:string, isGood:boolean, id?:number) {
+  isGood: boolean;
+  isRight: boolean = false;
+
+  constructor(texte: string, isGood: boolean, id?: number) {
     this.texte = texte;
     this.id = id;
     this.isGood = isGood;
   }
-
 }
