8cf71ef391b9a562a7300881da68e12789e55172
Evann Berthou <evann.berthou@gmail.com>
2023-01-07 20:09:52 +0000

    Nettoyage

--- src/app/Component/Creation/creation-editions/creation-editions.component.ts
+++ src/app/Component/Creation/creation-editions/creation-editions.component.ts
@@ -1,18 +1,7 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser";
-import testPdf from '../../../../assets/pdf/Test.pdf';
-import {Router} from "@angular/router";
-import {QCM} from "../../../Modeles/QCM";
-import {QcmService} from "../../../Services/qcm.service";
-import {QuestionService} from "../../../Services/question.service";
-import {EtudiantsService} from "../../../Services/etudiants.service";
-import {ClasseService} from "../../../Services/classe.service";
-import {GroupeService} from "../../../groupe.service";
-import {Groupe} from "../../../Modeles/GROUPE";
-import {Classe} from "../../../Modeles/CLASSE";
-import {MatSelectChange} from "@angular/material/select";
-import {FormControl} from "@angular/forms";
-import {CreationQCMComponent} from "../../../Pages/creation-qcm/creation-qcm.component";
+import { Component, Input, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
+import { QCM } from "../../../Modeles/QCM";
+import { QcmService } from "../../../Services/qcm.service";
 
 @Component({
   selector: 'app-creation-editions',
@@ -21,147 +10,39 @@ import {CreationQCMComponent} from "../../../Pages/creation-qcm/creation-qcm.com
 })
 export class CreationEditionsComponent implements OnInit {
   @Input() qcm!: QCM;
-  id : any = sessionStorage.getItem('ID');
-  
-  groupes : Groupe[] = [];
-  classes : Classe[] = [];
-  selectedClasse : any = null;
-  classesList: String[] = [];
-  selectedGroupe : any = null;
-  groupesList: String[] = [];
-  classesForm = new FormControl('');
-  groupesForm = new FormControl('');
-  public errormessage: string = "";
+  errormessage: string = "";
 
-  constructor(public sanitizer:DomSanitizer, public questionService:QuestionService, public qcmService:QcmService, public classeService:ClasseService,public groupeService : GroupeService, public creationQCMComponent :CreationQCMComponent) {
-    if(this.id != null){
-      this.classeService.getClassesFromUser(<number>this.id).subscribe(r=>{
-        r.forEach(classe=>{
-          this.classes.push(classe);
-          this.classesList.push(classe.nomClasse)
-        })
-      })
-    }
+  constructor(public sanitizer: DomSanitizer, public qcmService: QcmService) {
   }
 
-  pdf : SafeResourceUrl | null= null;
+  pdf: SafeResourceUrl | null = null;
   isGenerate: boolean = false;
   isGenerating: boolean = false;
-  url: any;
 
   ngOnInit(): void {
-    /*this.questionService.QCMActuel.subscribe(r=>{
-      this.qcm =r;
-    });
-    if(!this.questionService.isNotSaved.value)
-      this.qcmService.getPDFFromIdAndId(this.qcm?.idcreateur,this.qcm?.id).then(r=>{
-          if(r.size != 0){
-            let url = window.URL.createObjectURL(r);
-            this.pdf = this.sanitizer.bypassSecurityTrustResourceUrl(url);
-            this.isGenerate = true;
-          }
-          else{
-            this.isGenerate = false;
-            this.pdf = null;
-          }
-        }
-      )*/
-      this.qcmService.getPDFFromId(this.qcm).subscribe((r:Blob) => {
-        if(r.size > 0){
-          this.pdf = r;
-          let url = window.URL.createObjectURL(r);
-          this.pdf = this.sanitizer.bypassSecurityTrustResourceUrl(url);
-          this.isGenerate = true;
-        }
-      })
+    this.getPDF();
   }
 
   generateQCM() {
-    let requ;
-    /*if(!this.questionService.isNotSaved.value){
-      if (this.qcm != undefined) {
-        if (this.selectedClasse != undefined) {
-          if (this.selectedGroupe != undefined) {
-            requ = this.qcmService.generateNewQCM(this.qcm, this.selectedClasse.id, this.selectedGroupe.id);
-          }
-          else{
-            requ = this.qcmService.generateNewQCM(this.qcm, this.selectedClasse.id, null);
-          }
-          this.isGenerating = true;
-          requ.subscribe(r => {
-            if (r != null) {
-              this.isGenerate = true;
-              this.qcmService.getPDFFromIdAndId(this.qcm?.idcreateur, this.qcm?.id).then(r => {
-                  this.pdf = r;
-                  let url = window.URL.createObjectURL(r);
-                  this.pdf = this.sanitizer.bypassSecurityTrustResourceUrl(url);
-                }
-              )
-            } else {
-              this.isGenerate = false;
-              this.pdf = null;
-            }
-            this.isGenerating = false;
-          });
-        }
-        else{
-          this.errormessage = "Merci de sélectionner une classe et éventuellement un groupe avant de générer un QCM";
-          this.isGenerate = false;
-          this.pdf = null;
-        }
-        this.isGenerate = true;
-      }
-    }*/
-    requ = this.qcmService.generateNewQCM(this.qcm, undefined, null);
     this.isGenerating = true;
-    requ.subscribe((r: {exitstatus:number,pid:number}) => {
-      if (r.exitstatus == 0) {
-        this.isGenerate = true;
-        this.qcmService.getPDFFromId(this.qcm).subscribe((r:any) => {
-            this.pdf = r;
-            let url = window.URL.createObjectURL(r);
-            this.pdf = this.sanitizer.bypassSecurityTrustResourceUrl(url);
-          }
-        )
-      } else {
-        this.isGenerate = false;
-        this.pdf = null;
+
+    this.qcmService.generateNewQCM(this.qcm, undefined, undefined).subscribe(({ error }: {error: number}) => {
+      this.isGenerate = error === 0;
+      if (this.isGenerate) {
+        this.getPDF();
       }
       this.isGenerating = false;
     });
   }
-  /*
 
-  telechargerPdf() {
-    window.open(this.url);
-  }
-
-
-  onClasseChange($event: MatSelectChange) {
-    for(let i = 0 ; i < this.classes.length ; i++ ){
-      if(this.classes[i].nomClasse == $event.value){
-        this.selectedClasse = this.classes[i];
+  private getPDF(): void {
+    this.qcmService.getPDFFromId(this.qcm).subscribe((pdfBlob: Blob) => {
+      if (pdfBlob.size > 0) {
+        let url = window.URL.createObjectURL(pdfBlob);
+        this.pdf = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+        this.isGenerate = true;
       }
-    }
-
-    this.groupesList = []
-    if(this.selectedClasse != undefined){
-      this.groupeService.getGroupesFromUser(this.id,this.selectedClasse.id).subscribe(r => {
-
-        this.groupes = r;
-        for(let i = 0 ; i < r.length ; i++){
-          this.groupesList.push(r[i].nomGroupe)
-        }
-      })
-    }
+    })
   }
-
-  onGroupeChange($event: MatSelectChange) {
-    for(let i = 0 ; i<this.groupes.length ; i++){
-      if(this.groupes[i].nomGroupe == $event.value){
-        this.selectedGroupe = this.groupes[i];
-      }
-    }
-  }*/
 }
 
--- src/app/Pages/creation-qcm/creation-qcm.component.ts
+++ src/app/Pages/creation-qcm/creation-qcm.component.ts
@@ -1,12 +1,7 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from "@angular/router";
 import { QcmService } from "../../Services/qcm.service";
-import { QuestionService } from "../../Services/question.service";
 import { QCM } from "../../Modeles/QCM";
-import { Option } from "../../Modeles/OPTION";
-import { NotificationService } from "../../Services/notification.service";
-import { DialogDeleteComponent } from "../../Component/dialog-delete/dialog-delete.component";
-import { MatDialog } from "@angular/material/dialog";
 import { Observable } from 'rxjs';
 
 @Component({
@@ -17,72 +12,8 @@ import { Observable } from 'rxjs';
 export class CreationQCMComponent {
   qcm$: Observable<QCM>;
 
-  constructor(private dialog: MatDialog,
-    public route: ActivatedRoute,
-    private notificationService: NotificationService,
-    private questionService: QuestionService,
-    private qcmService: QcmService,
-    public router: Router) {
-
+  constructor(public route: ActivatedRoute, private qcmService: QcmService, public router: Router) {
     const id = this.route.snapshot.params['id'];
     this.qcm$ = this.qcmService.getQCMFromId(id)
-    console.log(this.qcm$.subscribe(r=> console.log(r)))
   }
-
-  /*
-  checkSave() {
-    if (this.isNotSaved) {
-      if (this.qcmLocal?.id) {
-        this.qcmLocal.categories.forEach(categorie => {
-          categorie.questions.forEach(question => {
-            if (!question.options)
-              question.options = new Options(question.typeDeQuestion, []);
-          })
-        })
-
-        this.qcmService.modifyQCM(this.qcmLocal).subscribe(res => {
-          this.notificationService.successMessage(this.qcmLocal?.titre + " SAUVEGARDÉ AVEC SUCCÈS");
-        });
-        this.questionService.isNotSaved.next(false);
-      }
-      else {
-        if (this.qcmLocal)
-          this.qcmLocal.titre = this.name;
-        this.qcmService.createNewQCM(this.qcmLocal!).subscribe(res => {
-          this.notificationService.successMessage(this.qcmLocal?.titre + " CRÉÉ AVEC SUCCÈS")
-          this.qcmService.getQCMFromId(res).subscribe(r => {
-            this.questionService.reloadQCM(r);
-            this.qcmLocal = r;
-          })
-        });
-        this.questionService.isNotSaved.next(false);
-      }
-    }
-  }
-
-  deleteQcm() {
-    const dialogRef = this.dialog.open(DialogDeleteComponent, {
-      data: {
-        title: 'Supprimer de façon permanente cette application?',
-        text: 'Attention vous êtes sur le point de supprimer définitivement un QCM ainsi que ses données du serveur !',
-        button: 'Supprimer',
-      },
-      disableClose: true
-    });
-    dialogRef.afterClosed().subscribe(result => {
-      if (result) {
-        if (this.qcmLocal?.id) {
-          this.qcmService.deleteQCM(this.qcmLocal).subscribe(res => this.notificationService.successMessage("Le QCM " + this.qcmLocal?.titre + " a bien été supprimé"));
-        }
-        this.router.navigate(["/"]);
-      }
-    });
-  }
-
-  ngAfterViewInit(): void {
-    if (this.qcmLocal?.id) {
-      this.questionService.isNotSaved.next(false);
-    }
-  }
-  */
 }
