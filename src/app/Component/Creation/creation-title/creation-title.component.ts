d047497ac65683425bc3bec556e23d6eb3a8de00
Krapoviche <63341122+Krapoviche@users.noreply.github.com>
2023-03-19 18:13:44 +0100

    deplacement save composant title

--- src/app/Component/Creation/creation-title/creation-title.component.ts
+++ src/app/Component/Creation/creation-title/creation-title.component.ts
@@ -1,4 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
+import { interval } from 'rxjs';
+import { QCM } from 'src/app/Modeles/QCM';
+import { QcmService } from 'src/app/Services/qcm.service';
 
 @Component({
   selector: 'app-creation-title',
@@ -8,9 +11,24 @@ import {Component, Input, OnInit} from '@angular/core';
 export class CreationTitleComponent implements OnInit {
   @Input() title : string = '';
   @Input() selector : string = '';
-  constructor() { }
+  saving: boolean = false
+  @Input() qcm! : QCM;
+
+
+  source = interval(15000);
+  subscription = this.source.subscribe(val => this.saveQCM());
+  constructor(private qcmService: QcmService) { }
 
   ngOnInit(): void {
   }
 
+  saveQCM(): void {
+    if(!this.saving){
+      this.saving = true;
+      this.qcmService.saveQCM(this.qcm).subscribe(_ => {
+        this.qcm =_;
+        this.saving = false;
+      });
+    }
+  }
 }
