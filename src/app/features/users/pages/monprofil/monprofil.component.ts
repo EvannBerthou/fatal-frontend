159e742e3caf1e29ef67a786c1eef99ec3338d74
Evann Berthou <evann.berthou@gmail.com>
2024-04-02 19:27:28 +0200

    Refactoring

--- src/app/features/users/pages/monprofil/monprofil.component.ts
+++ src/app/features/users/pages/monprofil/monprofil.component.ts
@@ -1,41 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {User} from "../../../../core/models/USER";
+import {AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators} from "@angular/forms";
+import {AuthenticationService} from "../../services/authentication.service";
+import {catchError} from "rxjs/operators";
 
 @Component({
   selector: 'app-monprofil',
   templateUrl: './monprofil.component.html',
   styleUrls: ['./monprofil.component.scss']
 })
-export class MonprofilComponent implements OnInit {
-  hide = true;
-  hide_2 = true;
-  hide_3 = true;
-  isModifying:Boolean = false
-  public username: any;
-  public firstname : any;
-  public secondname : any;
-  public email: any;
-  public id: any;
+export class MonprofilComponent {
+  user: User;
+  isModifying = false;
+  hidePassword = [true, true, true];
+  passwordForm: FormGroup;
+  passwordError: string | undefined;
 
-  constructor() { }
-
-  ngOnInit(): void {
-    this.username = sessionStorage.getItem("USERNAME")
-    this.firstname = sessionStorage.getItem("PRENOM")
-    this.secondname = sessionStorage.getItem("NOM")
-    this.email = sessionStorage.getItem("EMAIL")
-    this.id = sessionStorage.getItem("ID")
+  constructor(private fb: FormBuilder, private authenticationService: AuthenticationService) {
+    this.user = JSON.parse(sessionStorage.getItem("user") || '');
+    this.passwordForm = this.fb.group({
+      'current_password': ['', Validators.required],
+      'password': ['', Validators.required],
+      'password_confirmation': ['', Validators.required],
+    }, {validators: this.matchPasswords});
   }
 
-  modify():void{
-    this.isModifying = true ? this.isModifying==false : this.isModifying = false;
+  modify() {
+    this.isModifying = true;
   }
 
-  save():void{
-    this.isModifying = true ? this.isModifying==false : this.isModifying = false;
-    // this.etudiant.nom = this.nom;
-    // this.etudiant.prenom = this.prenom;
-    // this.etudiant.numero = this.numero;
-    // this.etudiant.classe = this.class;
-    // this.etudiant.group = this.group;
+  save() {
+    console.log('Saving');
+    this.isModifying = false;
+  }
+
+  changePassword() {
+    if (!this.passwordForm.valid) return;
+    this.authenticationService.changePassword(this.passwordForm.getRawValue()).pipe(
+      catchError(error => this.passwordError = error.error.errors.full_messages.join('.\n'))
+    ).subscribe(_ => this.passwordError = undefined);
+  }
+
+  private matchPasswords = (group: AbstractControl): ValidationErrors | null => {
+    const pass = group.get('password');
+    const confirm = group.get('password_confirmation');
+    if (!pass?.dirty || !confirm?.dirty) return null;
+    return pass.value === confirm.value ? null : {noMatch: true};
   }
 }
--- src/app/features/users/services/authentication.service.ts
+++ src/app/features/users/services/authentication.service.ts
@@ -1,21 +1,23 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { tap } from "rxjs/operators";
-import { User } from "src/app/core/models/USER";
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {tap} from "rxjs/operators";
+import {User} from "src/app/core/models/USER";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
-  user: User | undefined = undefined;
+  user: User | undefined;
 
   constructor(private http: HttpClient) {
     const user = sessionStorage.getItem('user');
-    this.user = user ? JSON.parse(user) as User : undefined;
+    if (user) {
+      this.user = JSON.parse(user);
+    }
   }
 
   authenticationService(username: string, password: string): any {
-    return this.http.post<any>('/auth/sign_in', { email: username, password }, { observe: 'response' })
+    return this.http.post<any>('/auth/sign_in', {email: username, password}, {observe: 'response'})
       .pipe(
         tap((response: any) => {
           this.user = response.body.data;
@@ -25,34 +27,16 @@ export class AuthenticationService {
       )
   }
 
+  changePassword(values: { current_password: string, password: string, password_confirmation: string }) {
+    return this.http.put<any>('/auth/password', {...values});
+  }
+
   logout(): void {
     sessionStorage.removeItem('user');
     sessionStorage.removeItem('token');
   }
 
-  isUserLoggedIn(): boolean {
-    return this.user !== undefined;
-  }
-
   getUser(): User | undefined {
     return this.user;
-  }
-
-  getLoggedInUserName(): string {
-    return this.user?.username || '';
-  }
-
-  getName() {
-    if (this.user === undefined) return '';
-    return `${this.user.username?.toUpperCase()} ${this.user.prenom}`
-  }
-
-  //TODO: Ne devrait pas être un string
-  getId(): string {
-    return this.user?.id.toString() || "0";
-  }
-
-  getAdmin(): boolean {
-    return this.user?.admin || false;
   }
 }
